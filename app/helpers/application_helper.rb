75163b2cb6e6cda2f91702478e218b926fe1e8ea
linhtn_57 <hotdog160993@gmail.com>
2016-06-01 22:39:43 +0700

    refactor code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def full_title(page_tile = '')
+    base_tile = "Ruby on Rails Tutorial Sample App"
+    page_tile.empty? ? base_tile : (page_tile + ' | ' + base_tile)
+  end
 end
